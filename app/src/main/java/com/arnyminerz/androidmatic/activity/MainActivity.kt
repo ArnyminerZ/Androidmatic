2b437177f337633e90bebbd3dc5d623e36a00207
Arnau Mora <arnyminer.z@gmail.com>
2022-09-25 12:09:04 +0200

    :sparkles: Added error card
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- app/src/main/java/com/arnyminerz/androidmatic/activity/MainActivity.kt
+++ app/src/main/java/com/arnyminerz/androidmatic/activity/MainActivity.kt
@@ -3,8 +3,10 @@ package com.arnyminerz.androidmatic.activity
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -14,10 +16,13 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Add
 import androidx.compose.material.icons.rounded.Home
 import androidx.compose.material.icons.rounded.Settings
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -26,7 +31,9 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.datastore.preferences.core.edit
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.arnyminerz.androidmatic.R
 import com.arnyminerz.androidmatic.data.NavigationBarItem
 import com.arnyminerz.androidmatic.storage.Keys
@@ -38,8 +45,10 @@ import com.arnyminerz.androidmatic.ui.components.SettingsCategory
 import com.arnyminerz.androidmatic.ui.components.SettingsItem
 import com.arnyminerz.androidmatic.ui.components.WeatherCard
 import com.arnyminerz.androidmatic.ui.theme.setThemedContent
+import com.arnyminerz.androidmatic.ui.viewmodel.ERROR_NONE
+import com.arnyminerz.androidmatic.ui.viewmodel.ERROR_SERVER_SEARCH
+import com.arnyminerz.androidmatic.ui.viewmodel.ERROR_TIMEOUT
 import com.arnyminerz.androidmatic.ui.viewmodel.MainViewModel
-import com.arnyminerz.androidmatic.utils.doAsync
 import com.arnyminerz.androidmatic.utils.launch
 import com.google.accompanist.pager.ExperimentalPagerApi
 import com.google.accompanist.pager.HorizontalPager
@@ -49,7 +58,6 @@ import com.google.accompanist.swiperefresh.rememberSwipeRefreshState
 import com.google.firebase.analytics.ktx.analytics
 import com.google.firebase.crashlytics.ktx.crashlytics
 import com.google.firebase.ktx.Firebase
-import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class, ExperimentalPagerApi::class)
@@ -128,17 +136,49 @@ class MainActivity : AppCompatActivity() {
                 launch(AddStationActivity::class)
             else {
                 val weatherMap = viewModel.weather
+                val error = viewModel.error
 
                 LazyColumn {
+                    item {
+                        AnimatedVisibility(visible = error != ERROR_NONE) {
+                            Card(
+                                modifier = Modifier
+                                    .fillMaxWidth(),
+                                colors = CardDefaults.cardColors(
+                                    containerColor = MaterialTheme.colorScheme.errorContainer,
+                                    contentColor = MaterialTheme.colorScheme.onErrorContainer,
+                                ),
+                            ) {
+                                Text(
+                                    text = stringResource(R.string.error_load_data),
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .padding(start = 8.dp, end = 8.dp, top = 8.dp),
+                                    style = MaterialTheme.typography.labelLarge,
+                                    fontWeight = FontWeight.Bold,
+                                    fontSize = 22.sp,
+                                )
+                                Text(
+                                    text = stringResource(
+                                        when (error) {
+                                            ERROR_SERVER_SEARCH -> R.string.error_server_reach
+                                            ERROR_TIMEOUT -> R.string.error_server_timeout
+                                            else -> R.string.error_unknown
+                                        }
+                                    ),
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .padding(start = 8.dp, end = 8.dp, bottom = 8.dp),
+                                )
+                            }
+                        }
+                    }
+
                     enabledStations?.let { enStations ->
                         items(enStations) { stationId ->
                             val station = stations.find { it.uid == stationId }
                             if (station != null) {
-                                try {
-                                    viewModel.loadWeather(station)
-                                } catch (e: Exception) {
-                                    // TODO: Show some message in the UI
-                                }
+                                viewModel.loadWeather(station)
 
                                 WeatherCard(station, weatherMap[stationId])
                             }
