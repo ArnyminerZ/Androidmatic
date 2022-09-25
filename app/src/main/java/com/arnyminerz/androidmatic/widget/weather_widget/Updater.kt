b4cde33881c2c7313efb78b088381db1e9f6b6fa
Arnau Mora <arnyminer.z@gmail.com>
2022-09-25 12:56:09 +0200

    :sparkles: Added `updateWeatherWidget`
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

---
+++ app/src/main/java/com/arnyminerz/androidmatic/widget/weather_widget/Updater.kt
@@ -0,0 +1,31 @@
+package com.arnyminerz.androidmatic.widget.weather_widget
+
+import android.content.Context
+import android.os.Bundle
+import androidx.annotation.WorkerThread
+import androidx.glance.GlanceId
+import androidx.glance.appwidget.state.updateAppWidgetState
+import com.google.firebase.analytics.ktx.analytics
+import com.google.firebase.ktx.Firebase
+import org.json.JSONObject
+import timber.log.Timber
+
+@WorkerThread
+suspend fun updateWeatherWidget(context: Context, glanceId: GlanceId) {
+    Timber.i("Updating widget $glanceId...")
+    updateAppWidgetState(context, glanceId) { preferences ->
+        val prefKey = WeatherWidget.preferencesKey(glanceId)
+        val data = preferences[prefKey]?.let {
+            WeatherWidgetData.fromJson(JSONObject(it))
+        }!!
+        val weather = data.station.fetchWeather(context)
+        preferences[prefKey] = data.copy(weather = weather).toJson().toString()
+
+        Firebase.analytics.logEvent(
+            "widget_update",
+            Bundle().apply {
+                putString("station", data.station.uid)
+            },
+        )
+    }
+}
