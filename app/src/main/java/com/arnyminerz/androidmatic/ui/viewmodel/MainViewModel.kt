dbc7f6a23fdedccfe990ecd7f589577ca3334509
Arnau Mora <arnyminer.z@gmail.com>
2022-09-25 12:08:32 +0200

    :sparkles: Added `error`
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- app/src/main/java/com/arnyminerz/androidmatic/ui/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/arnyminerz/androidmatic/ui/viewmodel/MainViewModel.kt
@@ -5,17 +5,42 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateMapOf
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.AndroidViewModel
+import com.android.volley.NoConnectionError
+import com.android.volley.TimeoutError
 import com.android.volley.VolleyError
 import com.arnyminerz.androidmatic.data.Station
 import com.arnyminerz.androidmatic.data.WeatherState
 import com.arnyminerz.androidmatic.singleton.DatabaseSingleton
 import com.arnyminerz.androidmatic.utils.launch
 import com.arnyminerz.androidmatic.utils.ui
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.map
 import timber.log.Timber
+
+/**
+ * There has not been any error.
+ * @author Arnau Mora
+ * @since 2020925
+ */
+const val ERROR_NONE = 0
+
+/**
+ * The server could not be reached, or the response is invalid.
+ * @author Arnau Mora
+ * @since 20220925
+ * @see NoConnectionError
+ */
+const val ERROR_SERVER_SEARCH = 1
+
+/**
+ * Usually the device doesn't have an active internet connection.
+ * @author Arnau Mora
+ * @since 20220925
+ * @see TimeoutError
+ */
+const val ERROR_TIMEOUT = 2
 
 class MainViewModel(app: Application) : AndroidViewModel(app) {
     private val databaseSingleton = DatabaseSingleton.getInstance(app)
@@ -55,6 +80,17 @@ class MainViewModel(app: Application) : AndroidViewModel(app) {
     val loadingTasks = mutableStateListOf<String>()
 
     /**
+     * Holds the error code for whenever there's an error while loading the data.
+     * @author Arnau Mora
+     * @since 20220925
+     * @see ERROR_NONE
+     * @see ERROR_SERVER_SEARCH
+     * @see ERROR_TIMEOUT
+     */
+    var error by mutableStateOf(0)
+        private set
+
+    /**
      * Loads the current weather for the given station into [weather]. The task progress can be
      * supervised with [loadingTasks].
      * @author Arnau Mora
@@ -78,16 +114,26 @@ class MainViewModel(app: Application) : AndroidViewModel(app) {
         IllegalArgumentException::class,
         NullPointerException::class,
     )
-    fun loadWeather(station: Station, force: Boolean = false) =
+    fun loadWeather(station: Station, force: Boolean = false, first: Boolean = true): Job? =
         if (force || (!weather.contains(station.uid) && !loadingTasks.contains(station.uid)))
             launch {
-                ui { loadingTasks.add(station.uid) }
-                Timber.d("Fetching weather data for $station...")
-                val stationWeather = station.fetchWeather(getApplication())
-                Timber.d("Weather data for $station is ready. Showing in UI...")
-                ui {
-                    weather[station.uid] = stationWeather
-                    loadingTasks.remove(station.uid)
+                try {
+                    ui {
+                        error = ERROR_NONE
+                        loadingTasks.add(station.uid)
+                    }
+                    Timber.d("Fetching weather data for $station...")
+                    val stationWeather = station.fetchWeather(getApplication())
+                    Timber.d("Weather data for $station is ready. Showing in UI...")
+                    ui { weather[station.uid] = stationWeather }
+                } catch (e: NoConnectionError) {
+                    ui { error = ERROR_SERVER_SEARCH }
+                    if (first)
+                        loadWeather(station, force, false)
+                } catch (e: TimeoutError) {
+                    ui { error = ERROR_TIMEOUT }
+                } finally {
+                    ui { loadingTasks.remove(station.uid) }
                 }
             }
         else null
