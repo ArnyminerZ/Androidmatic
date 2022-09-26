f341108b64da8e71f9e94e7051640df3d539761f
Arnau Mora <arnyminer.z@gmail.com>
2022-09-26 22:23:38 +0200

    :sparkles: Added wind direction
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- app/src/main/java/com/arnyminerz/androidmatic/ui/components/WeatherCard.kt
+++ app/src/main/java/com/arnyminerz/androidmatic/ui/components/WeatherCard.kt
@@ -20,6 +20,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringArrayResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -101,6 +102,9 @@ fun WeatherCard(weather: WeatherState?) {
                     val humValues = weather.humidityValues
                     val barValues = weather.barometerValues
                     val windValues = weather.windSpeedValues
+                    val windDirectionIndex = weather.windDirectionLocalizedIndex()
+                    val windDirection =
+                        stringArrayResource(R.array.wind_directions)[windDirectionIndex]
                     IconText(
                         "${tempValues.value}ºC (${tempValues.min}ºC - ${tempValues.max}ºC)",
                         icon = Icons.Rounded.Thermostat,
@@ -123,7 +127,7 @@ fun WeatherCard(weather: WeatherState?) {
                             .fillMaxWidth(),
                     )
                     IconText(
-                        "${windValues.value} kmph (${windValues.max} kmph)",
+                        "${windValues.value} kmph $windDirection (${windValues.max} kmph)",
                         icon = Icons.Rounded.Air,
                         contentDescription = stringResource(R.string.image_desc_wind),
                         modifier = Modifier
