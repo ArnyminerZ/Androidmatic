19c96c1342bb285d31189e31dd838941eb6fa7f3
Arnau Mora <arnyminer.z@gmail.com>
2022-10-03 15:07:33 +0200

    :sparkles: Added `scheduleIfNotRunning`, `get` and `UpdateDataOptions`
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- app/src/main/java/com/arnyminerz/androidmatic/App.kt
+++ app/src/main/java/com/arnyminerz/androidmatic/App.kt
@@ -1,20 +1,14 @@
 package com.arnyminerz.androidmatic
 
 import android.app.Application
-import androidx.work.Constraints
-import androidx.work.NetworkType
-import androidx.work.PeriodicWorkRequestBuilder
-import androidx.work.WorkManager
-import androidx.work.await
 import com.arnyminerz.androidmatic.data.providers.MeteoclimaticProvider
 import com.arnyminerz.androidmatic.data.providers.WeewxTemplateProvider
 import com.arnyminerz.androidmatic.data.providers.model.WeatherProvider
 import com.arnyminerz.androidmatic.utils.doAsync
-import com.arnyminerz.androidmatic.worker.UpdateDataWorker
+import com.arnyminerz.androidmatic.worker.UpdateDataWorker.Companion.scheduleIfNotRunning
 import com.google.android.material.color.DynamicColors
 import com.google.android.material.color.DynamicColorsOptions
 import timber.log.Timber
-import java.util.concurrent.TimeUnit
 
 class App : Application() {
     override fun onCreate() {
@@ -35,28 +29,6 @@ class App : Application() {
                 .build(),
         )
 
-        doAsync {
-            WorkManager.getInstance(this@App)
-                .takeIf { it.getWorkInfosByTag("weather_updater").await().isEmpty() }
-                ?.also { Timber.i("Scheduling weather update worker...") }
-                ?.enqueue(
-                    PeriodicWorkRequestBuilder<UpdateDataWorker>(
-                        15,
-                        TimeUnit.MINUTES,
-                        5,
-                        TimeUnit.MINUTES
-                    )
-                        .setConstraints(
-                            Constraints.Builder()
-                                .setRequiredNetworkType(NetworkType.CONNECTED)
-                                .build()
-                        )
-                        .addTag("weather_updater")
-                        .build()
-                )
-                ?: run {
-                    Timber.i("Won't schedule worker since already running.")
-                }
-        }
+        doAsync { scheduleIfNotRunning(applicationContext) }
     }
 }
--- app/src/main/java/com/arnyminerz/androidmatic/worker/UpdateDataWorker.kt
+++ app/src/main/java/com/arnyminerz/androidmatic/worker/UpdateDataWorker.kt
@@ -1,22 +1,96 @@
 package com.arnyminerz.androidmatic.worker
 
 import android.content.Context
+import androidx.annotation.WorkerThread
+import androidx.datastore.preferences.core.edit
 import androidx.glance.appwidget.GlanceAppWidgetManager
+import androidx.work.Constraints
 import androidx.work.CoroutineWorker
+import androidx.work.NetworkType
+import androidx.work.PeriodicWorkRequestBuilder
+import androidx.work.WorkManager
 import androidx.work.WorkerParameters
+import androidx.work.await
+import com.arnyminerz.androidmatic.storage.Keys
+import com.arnyminerz.androidmatic.storage.dataStore
 import com.arnyminerz.androidmatic.widget.weather_widget.WeatherWidget
 import com.arnyminerz.androidmatic.widget.weather_widget.updateWeatherWidget
 import timber.log.Timber
+import java.util.concurrent.TimeUnit
 
 class UpdateDataWorker(appContext: Context, workerParams: WorkerParameters) :
     CoroutineWorker(appContext, workerParams) {
+    companion object {
+        fun get(context: Context) =
+            WorkManager
+                .getInstance(context)
+                .getWorkInfosByTagLiveData(UpdateDataOptions.WorkerTag)
+
+        @WorkerThread
+        suspend fun scheduleIfNotRunning(context: Context) {
+            WorkManager.getInstance(context)
+                .takeIf { it.getWorkInfosByTag(UpdateDataOptions.WorkerTag).await().isEmpty() }
+                ?.also { Timber.i("Scheduling weather update worker...") }
+                ?.enqueue(
+                    PeriodicWorkRequestBuilder<UpdateDataWorker>(
+                        UpdateDataOptions.RepeatInterval,
+                        TimeUnit.MINUTES,
+                        UpdateDataOptions.FlexTimeInterval,
+                        TimeUnit.MINUTES
+                    )
+                        .setConstraints(
+                            Constraints.Builder()
+                                .setRequiredNetworkType(NetworkType.CONNECTED)
+                                .build()
+                        )
+                        .addTag(UpdateDataOptions.WorkerTag)
+                        .build()
+                )
+                ?: run {
+                    Timber.i("Won't schedule worker since already running.")
+                }
+        }
+    }
+
     override suspend fun doWork(): Result {
         GlanceAppWidgetManager(applicationContext)
             .getGlanceIds(WeatherWidget::class.java)
             .forEach { updateWeatherWidget(applicationContext, it) }
 
+        applicationContext.dataStore.edit {
+            it[Keys.LAST_WORKER_RUN] = System.currentTimeMillis()
+        }
+
         Timber.i("Ran weather update")
 
         return Result.success()
     }
+}
+
+/**
+ * Stores some options about how the [UpdateDataWorker] should be scheduled.
+ * @author Arnau Mora
+ * @since 20221003
+ */
+object UpdateDataOptions {
+    /**
+     * Defines how often (in minutes) the worker shall be executed.
+     * @author Arnau Mora
+     * @since 20221003
+     */
+    const val RepeatInterval: Long = 15
+
+    /**
+     * Defines the time flex interval for the scheduled worker.
+     * @author Arnau Mora
+     * @since 20221003
+     */
+    const val FlexTimeInterval: Long = 5
+
+    /**
+     * Defines the tag to use for identifying the worker in the WorkManager.
+     * @author Arnau Mora
+     * @since 20221003
+     */
+    const val WorkerTag = "weather_updater"
 }
